096eb4fa6e9f6ce310a4b17c00517c1bed258267
Bolu <techbrobolu@gmail.com>
2025-02-06 12:04:59 +0100

    fix 1

--- index.js
+++ index.js
@@ -18,16 +18,24 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
+  console.log(`Received message: ${text} from chat ID: ${chatId}`);
+
   // Echo the message back in the desired format
   bot.sendMessage(chatId, `You said '${text}'`);
+  
 
   // Log the event using Vercel Analytics
   analytics.track('MessageReceived', {
     chatId: chatId,
     message: text,
   });
+  res.sendStatus(200);
 });
 
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Bot is working!');
+});
 // Start the Express server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
